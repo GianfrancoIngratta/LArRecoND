2cf3b9fdfc0a21b5d4a6016571430ef4177ad414
Gianfranco Ingratta <ingratta@dunegpvm04.fnal.gov>
2025-11-24 08:47:00 -0600

    added io_group attribute to the LArSP class and fill it in PandoraInterface

--- test/PandoraInterface.cxx
+++ test/PandoraInterface.cxx
@@ -373,6 +373,8 @@ void ProcessSPEvents(const Parameters &parameters, const Pandora *const pPrimary
             const float voxelY = (*larsp->m_y)[isp];
             const float voxelZ = (*larsp->m_z)[isp];
             const float voxelE = (*larsp->m_charge)[isp];
+            const float voxel_io_group = (*larsp->m_io_group)[isp];
+            std::cout << "voxel ( " << voxelX << ", " << voxelY << ", " << voxelZ << ") io_group " << voxel_io_group << "\n";
 
             // Skip this hit if its coordinates or energy are NaNs
             if (std::isnan(voxelX) || std::isnan(voxelY) || std::isnan(voxelZ) || std::isnan(voxelE))
